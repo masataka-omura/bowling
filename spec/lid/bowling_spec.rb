12e29bfc076f4d445e0edafc7289c0327d150df5
EC2 Default User <ec2-user@ip-172-31-2-3.us-east-2.compute.internal>
2021-08-03 07:35:35 +0000

    エラーテスト

--- spec/lid/bowling_spec.rb
+++ spec/lid/bowling_spec.rb
@@ -120,6 +120,45 @@ describe "ボウリングのスコア計算" do
       end  
     end
   end
+  describe "フレームごとの合計" do
+    context "全ての投球で１ピンずつ倒した場合" do
+      it "１フレーム目の合計が２になること" do
+        add_many_scores(20, 1)
+        @game.calc_score
+        expect(@game.flame_score(1)).to eq 2
+      end
+    end
+    context "スペアをとった場合" do
+      it "スペアボーナスが加算されること" do
+         #第一フレーム
+        @game.add_score(3)
+        @game.add_score(7)
+        #第二フレーム
+        @game.add_score(4)
+        #以降すべてガーター
+        add_many_score(17, 0)
+        
+        @game.calc_score
+        
+        expect(@game.flame_score(1)).to eq 14
+      end  
+    end
+    context "ストライクを取った場合" do
+      it "ストライクボーナスが加算されること" do
+        #第一フレーム
+        @game.add_score(10)
+        #第二フレーム
+        @game.add_score(5)
+        @game.add_score(4)
+        #以降すべてガーター
+        add_many_score(17, 0)
+        
+        @game.calc_score
+        
+        expect(@game.flame_score).to eq 19
+      end
+    end  
+  end  
 end
 private
 def add_many_score(count, pins)
